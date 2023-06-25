8599aeca5de43fcbd55c7797a3c6a32ef85a9169
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-06-25 20:44:39 +0300

    hotel and room db created

--- src/main/java/com/eb/HotelManagementSystem/model/Room.java
+++ src/main/java/com/eb/HotelManagementSystem/model/Room.java
@@ -1,8 +1,76 @@
 package com.eb.HotelManagementSystem.model;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.NavigableMap;
 
 @Entity
+@Table(name = "t_room")
 public class Room
 {
+
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private int capacity;
+
+    @ManyToOne
+    @JoinColumn(name = "hotel_id", nullable = false)
+    private Hotel hotel;
+
+    public Room(Long id, String number, int capacity, Hotel hotel) {
+        this.id = id;
+        this.number = number;
+        this.capacity = capacity;
+        this.hotel = hotel;
+    }
+
+    public Room()
+    {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Hotel getHotel() {
+        return hotel;
+    }
+
+    public void setHotel(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", number='" + number + '\'' +
+                ", capacity=" + capacity +
+                //", hotel=" + hotel +
+                '}';
+    }
 }
