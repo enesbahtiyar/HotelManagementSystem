ebfa2c767764ec33ad1a7f4276a021a2eea38520
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-06-29 19:40:02 +0300

    Room save implementation

--- src/main/java/com/eb/HotelManagementSystem/main/HotelManagementSystemService.java
+++ src/main/java/com/eb/HotelManagementSystem/main/HotelManagementSystemService.java
@@ -2,20 +2,28 @@ package com.eb.HotelManagementSystem.main;
 
 import com.eb.HotelManagementSystem.repository.HotelRepository;
 import com.eb.HotelManagementSystem.repository.HotelRepositoryImplementation;
+import com.eb.HotelManagementSystem.repository.RoomRepository;
+import com.eb.HotelManagementSystem.repository.RoomRepositoryImplementation;
 import com.eb.HotelManagementSystem.service.HotelService;
 import com.eb.HotelManagementSystem.service.HotelServiceImplementation;
+import com.eb.HotelManagementSystem.service.RoomService;
+import com.eb.HotelManagementSystem.service.RoomServiceImplementation;
 
 import java.util.Scanner;
 
 public class HotelManagementSystemService
 {
-    private static Scanner scanner = new Scanner(System.in);
+    private final static Scanner scanner = new Scanner(System.in);
 
 
     public static void displayMenuHotelManagementSystem()
     {
         HotelRepository hotelRepository = new HotelRepositoryImplementation();
         HotelService hotelService = new HotelServiceImplementation(hotelRepository);
+
+
+        RoomRepository roomRepository= new RoomRepositoryImplementation();
+        RoomService roomService = new RoomServiceImplementation(roomRepository,hotelRepository);
 
         boolean exit = false;
         while (!exit)
@@ -35,7 +43,7 @@ public class HotelManagementSystemService
                     displayHotelOperationMenu(hotelService);
                     break;
                 case 2:
-                    displayRoomOperationMenu();
+                    displayRoomOperationMenu(roomService);
                     break;
                 case 3:
                     displayGuestOperationMenu();
@@ -70,29 +78,24 @@ public class HotelManagementSystemService
 
             switch (choice) {
                 case 1:
-                    //step 13e:saveHotel
                     System.out.println("==== Add a new hotel ====");
                     hotelService.saveHotel();
                     break;
                 case 2:
-                    // //step 14e:findHotelById
                     System.out.println("Enter the hotel ID: ");
                     Long hotelId = scanner.nextLong();
                     hotelService.findHotelById(hotelId);
                     break;
                 case 3:
-                    ////step 15e :deleteHotelById
                     System.out.print("Enter the hotel ID to delete: ");
                     Long deleteHotelId  =scanner.nextLong();
                     hotelService.deleteHotelById(deleteHotelId);
                     break;
                 case 4:
-                    //step 16e: findAllHotels
                     System.out.println("==== Find All Hotels ====");
                     hotelService.findAllHotels();
                     break;
                 case 5:
-                    //step 17e: updateHotelById
                     System.out.println("==== Update Hotel By ID ====");
 
                     break;
@@ -106,9 +109,44 @@ public class HotelManagementSystemService
         }
     }
 
-    public static void displayRoomOperationMenu()
+    public static void displayRoomOperationMenu(RoomService roomService)
     {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("==== Room Operations ====");
+            System.out.println("1. Add a new room");
+            System.out.println("2. Find Room By ID");
+            System.out.println("3. Delete Room By ID");
+            System.out.println("4. Find All Rooms");
+            System.out.println("5. Return to Main Menu");
+            System.out.print("Enter your choice: ");
 
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    System.out.println("==== Add New Room ====");
+                    roomService.saveRoom();
+                    break;
+                case 2:
+                    System.out.print("Enter the Room ID to Find: ");
+                    break;
+                case 3:
+                    System.out.println("==== Delete Room By ID ====");
+                    System.out.print("Enter the room ID to delete: ");
+                    break;
+                case 4:
+                    System.out.println("==== Find All Rooms ====");
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 
     public static void displayGuestOperationMenu()
--- src/main/java/com/eb/HotelManagementSystem/service/RoomServiceImplementation.java
+++ src/main/java/com/eb/HotelManagementSystem/service/RoomServiceImplementation.java
@@ -1,8 +1,64 @@
 package com.eb.HotelManagementSystem.service;
 
+import com.eb.HotelManagementSystem.exception.HotelResourceNotFoundException;
+import com.eb.HotelManagementSystem.model.Hotel;
+import com.eb.HotelManagementSystem.model.Room;
+import com.eb.HotelManagementSystem.repository.HotelRepository;
+import com.eb.HotelManagementSystem.repository.RoomRepository;
+
 import java.util.Scanner;
 
-public class RoomServiceImplementation
+public class RoomServiceImplementation implements RoomService
 {
-    private static Scanner scanner = new Scanner(System.in);
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private final RoomRepository roomRepository;
+    private final HotelRepository hotelRepository;
+
+    public RoomServiceImplementation(RoomRepository roomRepository, HotelRepository hotelRepository)
+    {
+        this.roomRepository = roomRepository;
+        this.hotelRepository = hotelRepository;
+    }
+
+    @Override
+    public Room saveRoom()
+    {
+        Room room = new Room();
+        System.out.println("Enter the room ID: ");
+        room.setId(scanner.nextLong());
+
+        System.out.println("Enter the Room Number: ");
+        room.setNumber(scanner.next());
+
+        System.out.println("Enter room capacity: ");
+        room.setCapacity(scanner.nextInt());
+        scanner.nextLine(); //consume next line
+
+        System.out.println("Please Enter Hotel Id: ");
+        Long hotelId = scanner.nextLong();
+        scanner.nextLine();
+
+        try
+        {
+            Hotel existingHotel = hotelRepository.findHotelById(hotelId);
+            if(existingHotel == null)
+            {
+                throw new HotelResourceNotFoundException("hotel not found with Id: " + hotelId);
+            }
+
+            room.setHotel(existingHotel);
+
+            Room saveRoom = roomRepository.saveRoom(room);
+
+            existingHotel.getRooms().add(saveRoom);
+
+            System.out.println("Room successfully added to the given hotel room id is: " + saveRoom.getId());
+        }
+        catch (HotelResourceNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return room;
+    }
 }
